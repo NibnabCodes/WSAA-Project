8858361d8914d5131939a3af02bb03fbbc9f09bd
Niamh Hogan <hniamhh1995@gmail.com>
2026-05-11 19:06:21 +0100

    Add config_template.py

---
+++ config_template.py
@@ -0,0 +1,15 @@
+# Fill in your own MySQL credentials and RAWG API key below.
+# This file is ignored by git to protect sensitive information. Do not share this file publicly.
+# Rename file to config.py after filling in your credentials.
+# Authors: Niamh Hogan
+
+mysql = {
+    'host': 'localhost',
+    'user': 'your_mysql_username',
+    'password': 'your_mysql_password',
+    'database': 'your_mysql_database_name'
+} 
+
+rawg = {
+    'your_rawg_api_key'
+}
